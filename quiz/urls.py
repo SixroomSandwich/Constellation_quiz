68caa388d9431ca36dda3683c0ec6a19dda1b5b7
SixroomSandwich <konyshev.md@phystech.edu>
2026-03-31 18:33:19 +0300

    feat: add constellation list and detail pages with styling

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('constellations/', views.constellation_list, name='constellation_list'),
+    path('constellation/<int:pk>/', views.constellation_detail, name='constellation_detail'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Constellation
 
 def home(request):
     return render(request, 'quiz/home.html')
+
+def constellation_list(request):
+    '''Страница со списком всех созвездий'''
+    constellations = Constellation.objects.all()
+    return render(request, 'quiz/constellation_list.html', {
+        'constellations': constellations
+    })
+
+def constellation_detail(request, pk):
+    '''Страница с детальной информацией о созвездии'''
+    constellation = get_object_or_404(Constellation, pk=pk)
+    return render(request, 'quiz/constellation_detail.html', {
+        'constellation': constellation
+    })
