e25961d12fa46590982989a38a12fd737b969f21
SixroomSandwich <konyshev.md@phystech.edu>
2026-03-31 16:20:58 +0300

    feat: add Contellation model and admin interface

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Constellation
 
-# Register your models here.
+@admin.register(Constellation)
+class ConstellationAdmin(admin.ModelAdmin):
+    list_display = ['name_ru', 'name_la', 'created_at']
+    search_fields = ['name_ru', 'name_la']
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Constellation(models.Model):
+    # Названия созвездий на русском и на латыни
+    name_ru = models.CharField(max_length=100, verbose_name="Название (рус.)")
+    name_la = models.CharField(max_length=100, verbose_name="Название (лат.)", blank=True)
+
+    # Информация о созвездии
+    description = models.TextField(verbose_name="Описание", blank=True)
+
+    # Картинка созвездия
+    image_url = models.URLField(verbose_name="Ссылка на созвездие", blank=True)
+
+    # Мета-информация
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    def __str__(self):
+        return self.name_ru
+    
+    class Meta:
+        verbose_name = "Созвездие"
+        verbose_name_plural = "созвездия"
+        ordering = ['name_ru']
